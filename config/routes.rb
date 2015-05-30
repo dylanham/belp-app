b657a310bc2abd98558ba0b97d1b83833175a04c
Dylan Ham <dylanham@gmail.com>
2015-05-30 12:05:46 -0600

    route updates for api as well as breweries show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
 
   get 'sign-out', to: 'sessions#destroy'
   get 'brewery_map', to: 'breweries#index'
+  get 'brewery_info', to: 'breweries#info'
 
   resources :users, only: [:show]
   resources :states, only: [:show]
+  resources :breweries, only: [:show]
 
   get "auth/failure", to: redirect('/')
   get "auth/twitter/callback", to: 'sessions#create'
-
 
 end
