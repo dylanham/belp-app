ee7234a13a94e3327247c50213eb253fa57da64c
Dylan Ham <dylanham@gmail.com>
2015-05-30 12:05:11 -0600

    user can view breweries show page with google map on it

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -4,4 +4,12 @@ class BreweriesController < ApplicationController
   def index
     respond_with(StateMap::AREAS)
   end
+
+  def show
+    @brewery = SecondBrewery.find(params[:id])
+  end
+
+  def info
+    respond_with(SecondBrewery.all)
+  end
 end
