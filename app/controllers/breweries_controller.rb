b812def991de7836e79ee8f870bea580d078bfe1
Dylan Ham <dylanham@gmail.com>
2015-05-31 08:05:38 -0600

    add comments to breweries

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -7,6 +7,8 @@ class BreweriesController < ApplicationController
 
   def show
     @brewery = SecondBrewery.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.where(second_brewery_id: params[:id])
   end
 
   def info
