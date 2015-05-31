682a4c67b244ebae91b4c7409f3f768be2049708
Dylan Ham <dylanham@gmail.com>
2015-05-31 15:00:58 -0600

    update for breweries show and comments controller

--- app/assets/javascripts/breweries_show.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  var query = $('#brewery-name').text().replace('&', ' ').split(' ').join('+')
-  var cityState = $('#city-state').text().trim().split(' ').join('+');
-  console.log(cityState);
-  console.log($('iframe').first().attr('src','https://www.google.com/maps/embed/v1/place?key=AIzaSyA5pNv0t4eXixPXpXSCtW0fGCXcQCllsng&q=' + query + ',' +cityState))
-})
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,15 @@ class CommentsController < ApplicationController
   before_action :set_brewery, only: [:create]
 
   def create
-    comment = Comment.new(params.require(:comment).permit(:content))
-    comment.second_brewery_id = @brewery.id
-    comment.save
-    redirect_to brewery_path(comment.second_brewery_id)
+    @comment = Comment.new(params.require(:comment).permit(:content))
+    @comment.second_brewery_id = @brewery.id
+    respond_to do |format|
+      if @comment.save
+        format.json { render json: @comment }
+      else
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
