d04cac54eda588cfe378dc5d673b6c079949fb57
Dylan Ham <dylanham@gmail.com>
2015-06-11 07:20:18 -0600

    Cleanup for unused code

--- app/controllers/second_brewery_api_controller.rb
+++
@@ -1,7 +0,0 @@
-class SecondBreweryApiController < ApplicationController
-  respond_to :json
-
-  def index
-    respond_with(SecondBrewery.all)
-  end
-end
--- app/models/second_brewery.rb
+++
@@ -1,2 +0,0 @@
-class SecondBrewery < ActiveRecord::Base
-end
