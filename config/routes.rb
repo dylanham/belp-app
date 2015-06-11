5969c033cda7a6ee2f671bbafd9c18c062f38291
Dylan Ham <dylanham@gmail.com>
2015-06-11 07:26:54 -0600

    removed unneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,14 @@ Rails.application.routes.draw do
   get 'sign-out', to: 'sessions#destroy'
   get 'brewery_map', to: 'breweries#index'
   get 'brewery_info', to: 'breweries#info'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
+  resources :brew_tours, only: [:index, :new]
   resources :users, only: [:show]
   resources :states, only: [:show]
-  resources :breweries, only: [:show]
-  resources :second_breweries do
+  resources :breweries, only: [:show] do
     resources :comments, only: [:create]
   end
 
