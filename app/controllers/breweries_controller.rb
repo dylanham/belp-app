c5b2b26a8ea565bae997d1cff6e83de004194edb
Dylan Ham <dylanham@gmail.com>
2015-06-11 07:23:43 -0600

    rename of second brewery to brewery

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -6,12 +6,12 @@ class BreweriesController < ApplicationController
   end
 
   def show
-    @brewery = SecondBrewery.find(params[:id])
+    @brewery = Brewery.find(params[:id])
     @comment = Comment.new
-    @comments = Comment.where(second_brewery_id: params[:id])
+    @comments = Comment.where(brewery_id: params[:id])
   end
 
   def info
-    respond_with(SecondBrewery.all)
+    respond_with(Brewery.all)
   end
 end
