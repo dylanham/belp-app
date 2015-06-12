b7abf64b08722f1610b6a86511b402b238f38976
Dylan Ham <dylanham@gmail.com>
2015-06-12 15:52:55 -0600

    add brewery average rating method

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,2 +1,11 @@
 class Brewery < ActiveRecord::Base
+  has_many :ratings
+  has_many :comments
+
+  def average_rating
+    if ratings.present?
+      sum_of_scores = ratings.map(&:score).sum
+      sum_of_scores / ratings.count
+    end
+  end
 end
