ff4ea9aef76c2b41c0591a388045445db9f90fde
Dylan Ham <dylanham@gmail.com>
2015-06-12 16:00:37 -0600

    update schema for brew tours

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610141324) do
+ActiveRecord::Schema.define(version: 20150611215341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brew_tours", force: :cascade do |t|
+    t.string   "origin_and_origin"
+    t.string   "query"
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "breweries", force: :cascade do |t|
     t.string   "name"
