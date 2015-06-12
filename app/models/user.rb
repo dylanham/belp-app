69d87754138c05adc54318059b78bc16ddeda160
Dylan Ham <dylanham@gmail.com>
2015-06-12 15:54:45 -0600

    users model checks for has ratings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :comments
+  has_many :ratings
+  has_many :brew_tours
+
   def self.from_omniauth(auth)
     find_by(provider: auth.provider, uid: auth.uid) || create_from_omniauth(auth)
   end
@@ -11,5 +15,9 @@ class User < ActiveRecord::Base
       oauth_token: auth["credentials"]["token"],
       oauth_expires_at: Time.at(auth["credentials"]["expires_at"])
     })
+  end
+
+  def has_rated_brewery?(brewery)
+    brewery.ratings.map(&:user_id).include?(self.id)
   end
 end
