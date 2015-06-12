f81559b0ddce4d5e196242e78a4c191b96ee63f1
Dylan Ham <dylanham@gmail.com>
2015-06-12 15:51:02 -0600

    add create action for persisting tours to database

--- app/controllers/brew_tours_controller.rb
+++ app/controllers/brew_tours_controller.rb
@@ -1,4 +1,21 @@
 class BrewToursController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
   def index
   end
+
+  def show
+    @brew_tour = BrewTour.find(params[:id])
+  end
+
+  def create
+    brew_tour = BrewTour.new(params.require(:brew_tour).permit(:origin_and_origin, :query, :title))
+    brew_tour.user_id = current_user.id
+    if brew_tour.save
+      redirect_to brew_tours_path
+    else
+      redirect_to brew_tours_path
+    end
+  end
+
+
 end
