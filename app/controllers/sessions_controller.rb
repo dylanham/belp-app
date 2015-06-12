abe7b2ee2096ee6b71cf6d513ef04fc81083c102
Dylan Ham <dylanham@gmail.com>
2015-06-12 15:52:07 -0600

    change session.clear to nil out the user id to keep return to information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
     flash[:notice] = 'Welcome!'
-    redirect_to root_path
+    redirect_to session[:return_to] || root_path
   end
 
   def destroy
-    session.clear
+    session[:user_id] = nil
     flash[:notice] = 'Goodbye'
-    redirect_to root_path
+    redirect_to session[:return_to] || root_path
   end
 end
