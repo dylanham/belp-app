5b854dc93b7e5040ca5ea8871aa60cd831659dce
Dylan Ham <dylanham@gmail.com>
2015-06-13 09:12:07 -0600

    update users show page for showing of brew tours

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+  $(document).on('click', '.show-hide', function(){
+    $('.ratings').toggleClass('hidden');
+    if ($('.ratings').hasClass('hidden')){
+      $(this).text('View your breweries')
+    } else {
+      $(this).text('Hide your breweries');
+    }
+  })
+})
