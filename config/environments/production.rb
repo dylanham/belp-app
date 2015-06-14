b004698a13666df422ab126f0537693e7f370bf0
Dylan Ham <dylanham@gmail.com>
2015-06-13 19:57:35 -0600

    update for background images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
   # Eager load code on boot. This eager loads most of Rails and
